0f40a88d89021f28651e138ac7cf9558390cf8ea
adityarifkidev <adityrifki.dev@gmail.com>
2025-05-28 17:27:35 +0700

    first commit

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi everyone, I am <span className="purple">Aditya Rifki Nugroho. </span><br />
+            I’m currently working as a Front-End Developer and UI/UX Designer at Solusi Digital Kamu.
+            <br />
+           I’ve completed several projects, including building landing pages using React and designing hi-fi interfaces for the Sahabat Tani Madiun app.
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Games
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+
+          <p style={{ color: "white" }}>
+            "Strive to build things that make a difference!"{" "}
+          </p>
+          
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
